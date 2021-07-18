3a5b42a37c956da089f4fd7a11917c24d29c51d4
moba1 <moba2@protonmail.com>
2021-07-18 13:07:17 +0900

    add usage

--- main.go
+++ main.go
@@ -6,18 +6,44 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path"
 	"strconv"
 
 	"github.com/olekukonko/tablewriter"
 	"golang.org/x/text/unicode/runenames"
 )
 
-func main() {
+var reader func(*bufio.Reader) (rune, []byte, error)
+
+func parseCmd() {
 	const (
 		utf8CmdName  = "utf8"
 		utf16CmdName = "utf16"
 		utf32CmdName = "utf32"
 	)
+
+	flag.Usage = func() {
+		prog := path.Base(os.Args[0])
+		stmts := []string{
+			fmt.Sprintf("Usage of %s:", prog),
+			fmt.Sprintf("  %s <subcommand>", prog),
+			fmt.Sprintf("  %s -help", prog),
+			"Sub commands:",
+			fmt.Sprintf("  %s\tdump UTF-8", utf8CmdName),
+			fmt.Sprintf("  %s\tdump UTF-16", utf16CmdName),
+			fmt.Sprintf("  %s\tdump UTF-32", utf32CmdName),
+			"Options:",
+			"  -help",
+			"       show help",
+		}
+		for _, stmt := range stmts {
+			fmt.Fprintln(flag.CommandLine.Output(), stmt)
+		}
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	args := flag.Args()
+
 	utf8Cmd := flag.NewFlagSet(utf8CmdName, flag.ExitOnError)
 	utf16Cmd := flag.NewFlagSet(utf16CmdName, flag.ExitOnError)
 	utf32Cmd := flag.NewFlagSet(utf32CmdName, flag.ExitOnError)
@@ -25,45 +51,81 @@ func main() {
 		subCmd     string
 		subCmdArgs []string
 	)
-	if len(os.Args) < 2 {
+	if len(args) < 2 {
 		subCmd = utf8CmdName
 		subCmdArgs = []string{}
 	} else {
-		subCmd = os.Args[1]
-		subCmdArgs = os.Args[2:]
+		subCmd = args[0]
+		subCmdArgs = args[1:]
 	}
-	var (
-		reader      func(*bufio.Reader) (rune, []byte, error)
-		parseEndian = func(endianHolder *Endian, s string) error {
-			switch s {
-			case "Big":
-				*endianHolder = BigEndian
-			case "Little":
-				*endianHolder = LittleEndian
-			default:
-				return fmt.Errorf("invalid endian: %s", s)
-			}
-			return nil
+	var parseEndian = func(endianHolder *Endian, s string) error {
+		switch s {
+		case "Big":
+			*endianHolder = BigEndian
+		case "Little":
+			*endianHolder = LittleEndian
+		default:
+			return fmt.Errorf("invalid endian: %s", s)
 		}
-	)
+		return nil
+	}
 	switch subCmd {
 	case utf8CmdName:
+		utf8Cmd.Usage = func() {
+			stmts := []string{
+				fmt.Sprintf("Usage of %s:", utf8CmdName),
+				fmt.Sprintf("  %s [option]", utf8CmdName),
+				"Options:",
+				"  -help",
+				"        show help",
+			}
+			for _, stmt := range stmts {
+				fmt.Fprintln(utf8Cmd.Output(), stmt)
+			}
+			utf8Cmd.PrintDefaults()
+		}
 		utf8Cmd.Parse(subCmdArgs)
 		reader = ReadUtf8Char
 	case utf16CmdName:
 		var endian Endian = BigEndian
-		utf16Cmd.Func("endian", "UTF16 endian", func(s string) error {
+		utf16Cmd.Func("endian", "UTF16 endian. default is `Big` (value: Big|Little)", func(s string) error {
 			return parseEndian(&endian, s)
 		})
+		utf16Cmd.Usage = func() {
+			stmts := []string{
+				fmt.Sprintf("Usage of %s:", utf16CmdName),
+				fmt.Sprintf("  %s [option]", utf16CmdName),
+				"Options:",
+				"  -help",
+				"        show help",
+			}
+			for _, stmt := range stmts {
+				fmt.Fprintln(utf16Cmd.Output(), stmt)
+			}
+			utf16Cmd.PrintDefaults()
+		}
 		utf16Cmd.Parse(subCmdArgs)
 		reader = func(buf *bufio.Reader) (rune, []byte, error) {
 			return ReadUtf16Char(endian, buf)
 		}
 	case utf32CmdName:
 		var endian Endian = BigEndian
-		utf32Cmd.Func("endian", "UTF32 endian", func(s string) error {
+		utf32Cmd.Func("endian", "UTF32 endian. default is `Big` (value: Big|Little)", func(s string) error {
 			return parseEndian(&endian, s)
 		})
+		utf32Cmd.Usage = func() {
+			stmts := []string{
+				fmt.Sprintf("Usage of %s:", utf32CmdName),
+				fmt.Sprintf("  %s [option]", utf32CmdName),
+				"Options:",
+				"  -help",
+				"        show help",
+			}
+			for _, stmt := range stmts {
+				fmt.Fprintln(utf32Cmd.Output(), stmt)
+			}
+			utf32Cmd.PrintDefaults()
+		}
 		utf32Cmd.Parse(subCmdArgs)
 		reader = func(buf *bufio.Reader) (rune, []byte, error) {
 			return ReadUtf32Char(endian, buf)
@@ -72,6 +134,10 @@ func main() {
 		println(fmt.Sprintf("invalid command: %s", subCmd))
 		os.Exit(1)
 	}
+}
+
+func main() {
+	parseCmd()
 
 	buf := bufio.NewReader(os.Stdin)
 	runeTable := tablewriter.NewWriter(os.Stdout)
